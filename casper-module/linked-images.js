ea9de2db351ebad79dadcac2b079223b27377b82
William Gates <accessequals@gmail.com>
2015-09-15 09:04:36 +0100

    added image capture to linked-images

--- casper-module/linked-images.js
+++ casper-module/linked-images.js
@@ -4,6 +4,7 @@ var url = system.args[4];
 var fs = require('fs');
 // for JSON
 var output = [];
+var counter = 0;
 
 // filename
 var fname = new Date().getTime() + '.json';
@@ -16,12 +17,20 @@ casper.start(url, function() {
 
 casper.then(function() {
 	this.each(links, function(self, image) {
-		if (image.attributes.alt === undefined) {
+		if (image.attributes.alt === undefined) { // no alt text
 			output.push({ 
 				WCAG: '2.4.4-1: Linked image has missing alt text.',
 				message: 'Linked image has no alt text',
-				image: image.attributes.src
+				image: image.attributes.src,
+				imageID: 'image-' + counter
 			});
+			this.capture('image-' + counter + '.png', {
+				top: image.y,
+		        left: image.x,
+		        width: image.width,
+		        height: image.height
+			});
+			counter++;
 		} 
 	});
 });
