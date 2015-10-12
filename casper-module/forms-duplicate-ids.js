5626dc48eb721a2882ff43a292b230a1e507967d
William Gates <info@viadesign.co.uk>
2015-10-12 15:21:54 -0400

    Created proxies module for underscore

--- casper-module/forms-duplicate-ids.js
+++ casper-module/forms-duplicate-ids.js
@@ -2,7 +2,8 @@ var casper = require('casper').create();
 var system = require('system');
 var url = system.args[4];
 var fs = require('fs');
-var _ = require('underscore');
+//var _ = require('underscore');
+var _ = require('../proxies/underscore');
 
 var test = require('tests/test-module.js');
 test.test();
